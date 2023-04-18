d00fd3419752283c0fbff2d1988505b0dd776a51
Dave van der Meer <dave.vandermeer@uni.lu>
2023-04-18 10:04:11 +0200

    Add python launch file to spawn robot

--- ros2_ws/src/leorover_simulator/leorover_gazebo/launch/leorover_spawn_robot.launch.py
+++ ros2_ws/src/leorover_simulator/leorover_gazebo/launch/leorover_spawn_robot.launch.py
@@ -58,17 +58,17 @@ def generate_launch_description():
                 ],
                 # fmt: on
             ),
-            # Node(
-            #     name="joint_state_broadcaster_spawner",
-            #     package="controller_manager",
-            #     executable="spawner",
-            #     arguments=["joint_state_broadcaster"],
-            # ),
-            # Node(
-            #     name="diff_drive_controller_spawner",
-            #     package="controller_manager",
-            #     executable="spawner",
-            #     arguments=["diff_drive_controller"],
-            # ),
+            Node(
+                name="joint_state_broadcaster_spawner",
+                package="controller_manager",
+                executable="spawner",
+                arguments=["joint_state_broadcaster"],
+            ),
+            Node(
+                name="diff_drive_controller_spawner",
+                package="controller_manager",
+                executable="spawner",
+                arguments=["diff_drive_controller"],
+            ),
         ]
     )
