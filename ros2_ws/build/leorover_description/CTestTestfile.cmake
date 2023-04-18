946094bacbe5520f553bef108a7c146d9fd5f314
Dave van der Meer <dave.vandermeer@uni.lu>
2023-04-18 14:53:10 +0200

    Clean workspace

--- ros2_ws/build/leorover_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dave/Documents/docker/leo_sim_docker/ros2_ws/src/leorover_description
-# Build directory: /home/dave/Documents/docker/leo_sim_docker/ros2_ws/build/leorover_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
